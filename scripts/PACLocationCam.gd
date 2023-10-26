5ada24231e330f67d18f22057cde07928dc9cf5b
JD Calvelli <jdcalvelli@10-18-194-83.dynapool.wireless.nyu.edu>
2023-10-26 16:05:06 -0400

    adding camera lerping

---
+++ scripts/PACLocationCam.gd
@@ -0,0 +1,29 @@
+extends Camera2D
+
+func _process(delta):
+	_cameraSway(delta)
+
+### helper funcs
+
+func _cameraSway(delta:float):
+	# when i try to roll this into one vector 2 based call it doesnt work
+	# probably because if it breaks clamp on one of the vector 2 it stops the whole thing
+	# could rewrite into an on input + tween combination later
+	position.x = lerp(
+		position.x, 
+		clamp(
+			get_local_mouse_position().x,
+			-1 * get_viewport_rect().size.x / 8,
+			get_viewport_rect().size.x / 8
+		),
+		delta
+	)
+	position.y = lerp(
+		position.y,
+		clamp(
+			get_local_mouse_position().y,
+			-1 * get_viewport_rect().size.y / 8,
+			get_viewport_rect().size.y / 8
+		),
+		delta
+	)

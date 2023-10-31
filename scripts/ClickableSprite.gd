9b057b88eb908ecc86e37c748129a0e63e881691
JD Calvelli <jdcalvelli@10-18-194-83.dynapool.wireless.nyu.edu>
2023-10-31 16:20:34 -0400

    adding dialogue plugin and basic clickable obj integration

--- scripts/ClickableSprite.gd
+++ scripts/ClickableSprite.gd
@@ -1,11 +1,19 @@
 extends AnimatedSprite2D
 
+@export var stitchName : String
+var dialogueResource : Resource
+
 func _ready():
 	# internal signal connection
 	$Area2D.input_event.connect(_on_input)
+	# grab the current dialogue resource from PACLocation
+	# should be a signal? signal up, reference down?
+	dialogueResource = get_parent().PACLocationDialogue
+	
 
 ### signal callbacks
 func _on_input(viewport:Node, event:InputEvent, shape_idx:int):
 	if event.is_action_pressed("mouseClick"):
-		print("mouse clicked")
 		# this should trigger dialogue state basically
+		# right now utilizing the example dialogue balloon, could change later
+		DialogueManager.show_example_dialogue_balloon(dialogueResource, stitchName)

4587b0b81f9a9cc4074ce6dd15732951a0697656
JD Calvelli <jdcalvelli@10-18-194-83.dynapool.wireless.nyu.edu>
2023-11-02 13:40:42 -0400

    gating the picnic basket option until after other options
    also included a slight refactor to be calling dialogue manager from scene level, not from object level

--- scripts/ClickableObj.gd
+++ scripts/ClickableObj.gd
@@ -1,19 +1,19 @@
 extends AnimatedSprite2D
+class_name ClickableObject
 
+@export var isGated : bool
 @export var stitchName : String
-var dialogueResource : Resource
+
+signal isAccessed(emitter:ClickableObject)
 
 func _ready():
 	# internal signal connection
 	$Area2D.input_event.connect(_on_input)
-	# grab the current dialogue resource from PACLocation
-	# should be a signal? signal up, reference down?
-	dialogueResource = get_parent().PACLocationDialogue
 	
 
 ### signal callbacks
 func _on_input(viewport:Node, event:InputEvent, shape_idx:int):
 	if event.is_action_pressed("mouseClick"):
 		# this should trigger dialogue state basically
-		# right now utilizing the example dialogue balloon, could change later
-		DialogueManager.show_example_dialogue_balloon(dialogueResource, stitchName)
+		# getting handled in governing scene
+		isAccessed.emit(self)
--- scripts/pac_location.gd
+++ scripts/pac_location.gd
@@ -1,3 +1,30 @@
 extends Node2D
 
 @export var PACLocationDialogue : Resource
+
+var numClickables : int
+var ClickedObjects : Array
+
+func _ready():
+	for child in get_children():
+		if child is ClickableObject:
+			numClickables += 1
+			child.isAccessed.connect(_onClickHandle)
+
+func _onClickHandle(emitter:ClickableObject):	
+
+	# if the current object is the last to be clicked
+	if ClickedObjects.size() == numClickables - 1:
+		# ungate it
+		emitter.isGated = false
+	
+	# if the current object isn't gated
+	if !emitter.isGated:
+		# append it to the array
+		ClickedObjects.append(emitter)
+		# call the dialogue
+		# right now utilizing the example dialogue balloon, could change later
+		DialogueManager.show_example_dialogue_balloon(
+			PACLocationDialogue, 
+			emitter.stitchName
+		)

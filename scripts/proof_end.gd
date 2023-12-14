652be9af97b1ae627cb0d60874bc98fa8df2bfed
Devinne Moses <devinnemoses@10-18-141-196.dynapool.wireless.nyu.edu>
2023-12-14 14:53:07 -0500

    timing and audio adjustments

--- scripts/proof_end.gd
+++ scripts/proof_end.gd
@@ -3,11 +3,13 @@ extends Node2D
 var spaceLock = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_process(false)
 	cutFade()
 	$Background.hide()
 	$LabelSpace.hide()
 	$LabelSpace2.hide()
 	await get_tree().create_timer(3).timeout
+	set_process(true)
 	$LabelSpace.show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

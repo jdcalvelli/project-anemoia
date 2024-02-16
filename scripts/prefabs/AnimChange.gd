65ace582160991f5af622d1af5aebc8f166b9bbc
JD Calvelli <jdcalvelli@10-18-153-13.dynapool.wireless.nyu.edu>
2024-02-16 17:51:58 -0500

    enabling repeatability of animation if necessary

--- scripts/prefabs/AnimChange.gd
+++ scripts/prefabs/AnimChange.gd
@@ -8,8 +8,17 @@ func _ready():
 # then change the frame to equal whatever the frame signal number is
 func _on_change_anim_state(animState: int):
 	# six gates, but twelve frames devinne makes
+	
+	# if we havent reached the end of the animation, move it forward
 	if self.frame != self.sprite_frames.get_frame_count("default") - 1:
 		self.frame += 1
 		await get_tree().create_timer(0.1).timeout
 		self.frame += 1
 		print(self.frame)
+	else:
+		# if we have reached the end of the animation
+		# and if we havent reached the end of required actions
+		if GameManager.currentShot.numActionsTaken != GameManager.currentShot.numRequiredActions:
+			# reset the frame
+			# so that it we can repeat the animation
+			self.frame = 1

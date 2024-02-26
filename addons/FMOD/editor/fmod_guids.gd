6bfe9bf91ea040b08c86814cdb9e8e4863a156eb
JD Calvelli <jdcalvelli@10-18-153-13.dynapool.wireless.nyu.edu>
2024-02-25 20:59:23 -0500

    adding fmod integration
    updating gitignore to ignore unnecessary files

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,15 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'imogen-fmod.fspro'
+#
+
+class_name FMODGuids
+
+class Busses:
+	const MASTER_BUS = "{e700db10-905d-47fc-a252-f4c32cfc0906}"
+	const REVERB = "{ad704468-2335-47f2-9767-7978f556895c}"
+
+class Banks:
+	const MASTER = "{1ed4ac8a-8f90-4183-8494-65525f8bd933}"
+

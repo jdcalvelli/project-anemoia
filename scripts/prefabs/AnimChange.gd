3acee6243f4f2f97e2166b3d608dbbe16e6763b2
JD Calvelli <jdcalvelli@JDs-MacBook-Pro.local>
2024-03-01 17:53:08 -0500

    adding slight film jitter

--- scripts/prefabs/AnimChange.gd
+++ scripts/prefabs/AnimChange.gd
@@ -1,8 +1,21 @@
 extends AnimatedSprite2D
 
+var maxJitterVal:Vector2 = Vector2(2, 2)
+var frameCounter:int = 0
+
 # this needs to receive the signal for anim change
 func _ready():
 	EventBus.changeAnimState.connect(_on_change_anim_state)
+
+# update to be able to do the jitter animation
+func _physics_process(delta):
+	# every twelve frames do the jitter
+	if frameCounter % 12 == 0:
+		print("twelve frame")
+		# set the position of this image to some random value between 0 and maxjitterval
+		position = Vector2(randi_range(0, maxJitterVal.x + 1), randi_range(0, maxJitterVal.y + 1))
+	# increment frame counter
+	frameCounter += 1
 
 # if number of frames on the frameresource is bigger than 1
 # then change the frame to equal whatever the frame signal number is
